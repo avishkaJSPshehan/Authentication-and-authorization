a99d101a266a0e85f29641c0202eed57e7442a68
JAYASIRI S.P.A.S. <ict21860@fot.sjp.ac.lk>
2024-12-13 01:38:55 +0530

    Database Setup

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
+
 import { connectDB } from "./db/connectDB.js";
+import authRouter from "./routes/auth.route.js"
 
 dotenv.config();
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World 123456!");
+  res.send("Authentication and Authorization");
 });
+
+app.use("/api/auth",authRouter)
 
 app.listen(5000, () => {
   connectDB();
