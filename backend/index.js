354d13e0bd798f78f59b80ee671ded5f3740ce01
JAYASIRI S.P.A.S. <ict21860@fot.sjp.ac.lk>
2024-12-19 01:23:54 +0530

    Add: Check Auth Function

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import cookieParser from "cookie-parser";
 import { connectDB } from "./db/connectDB.js";
 import authRouter from "./routes/auth.route.js";
 
@@ -9,6 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRouter);
 
