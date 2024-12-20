12060f4a88f23f7502639714bd92fd9bd65c772e
JAYASIRI S.P.A.S. <ict21860@fot.sjp.ac.lk>
2024-12-20 19:13:17 +0530

    Setting up configurations

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./db/connectDB.js";
 import authRouter from "./routes/auth.route.js";
@@ -7,6 +8,8 @@ import authRouter from "./routes/auth.route.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use(express.json());
 app.use(cookieParser());
